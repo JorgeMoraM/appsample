b2841e2d978d795bfde271779584698b57d61733
Jorge Mora <jorgemoramanzano@gmail.com>
2018-10-14 23:44:53 -0500

    added readme

--- app.cpp
+++ app.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 
 using namespace std;
+
+void mergeSort(int* arr,int lastind){
+int i,j,aux=0;
+for(i=1;i<lastind;++i){
+    aux=arr[i];
+    for(j=i;j>0&&aux<arr[j-1];--j){
+        arr[j]=arr[j-1];
+        j=j-1;
+    }
+    if(i!=j){
+        arr[j]=aux;
+    }
+}
+for (int i=0;i<lastind;++i){
+    cout << " "<<arr[i]<<" ";
+}
+}
+
+
 int main(){
-cout << "Hello";
+    int arr[]={2,5,1,8,1,9,7,4,3,5};
+    int lastind=9;
+    mergeSort(arr,lastind);
+
 return 0;
 }
